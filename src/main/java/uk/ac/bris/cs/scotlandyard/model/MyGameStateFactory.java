b2e5262592e0a19ab3f1b102b1a0c431aa5ad1bd
ykmhere <zt22740@bristol.ac.uk>
2023-03-16 15:16:06 +0000

    changes

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -27,20 +27,24 @@ public final class MyGameStateFactory implements Factory<GameState> {
 	private final class MyGameState implements GameState {
 		private GameSetup setup;
 		private ImmutableSet<Piece> remaining;
+
 		// hold the travel log and count the moves Mr has taken
 		private ImmutableList<LogEntry> log;
+
 		// hold the MrX player
 		private Player mrX;
+
 		// hold the detectives
 		private List<Player> detectives;
-		// calls all players
-		private List<Player> allPlayers;
+
 		// hold the currently possible/available moves
 		private ImmutableSet<Move> moves;
+
 		// hold the current winner(s)
 		private ImmutableSet<Piece> winner;
 
-
+		// calls all players
+		private ImmutableSet<Player> allPlayers;
 
 		private MyGameState(
 				final GameSetup setup,
@@ -222,94 +226,78 @@ public final class MyGameStateFactory implements Factory<GameState> {
 		@Nonnull @Override
 		public GameState advance(Move move) {
 			if(!moves.contains(move)) throw new IllegalArgumentException("Illegal move: "+move);
-			List<LogEntry> updatedLog = new ArrayList<>(log);
-			List<Player> updatedDetective = new ArrayList<>();
-			Player updatedMrX;
+			List<LogEntry> updateLog = new ArrayList<>(log);
+			List<Player> updateDetective = new ArrayList<>();
+			Player updateMrX;
 			List<Piece> oldRemaining = new ArrayList<>(remaining);
-			List<Piece> updatedRemaining = new ArrayList<>();
+			List<Piece> updateRemaining = new ArrayList<>();
 			Player currentPlayer = getCurrentPlayer(move.commencedBy());
 
-			Move.Visitor<GameState> visitor = new Move.Visitor<>() {
+			Move.Visitor<Player> visitor = new Move.Visitor<Player>() {
 				@Override
-				public GameState visit(Move.SingleMove move) {
-					currentPlayer.use(move.ticket).at(move.destination);
+				public Player visit(Move.SingleMove move) {
 					// when the current player is Mr X
 					if(currentPlayer.isMrX()) {
 						// check if mr x's moves are revealed
 						if(setup.moves.get(log.size())) {
 							// revealed moves
-							updatedLog.add(LogEntry.reveal(move.ticket, move.destination));
+							updateLog.add(LogEntry.reveal(move.ticket, move.destination));
 						}
 						// hidden moves
-						else updatedLog.add(LogEntry.hidden(move.ticket));
+						else updateLog.add(LogEntry.hidden(move.ticket));
 					}
 					else {
 						// give mr x with new ticket from detectives
 						mrX = mrX.give(move.tickets());
 					}
-					return new MyGameState(setup, remaining, ImmutableList.copyOf(updatedLog), mrX, detectives);
+					return currentPlayer.use(move.ticket).at(move.destination);
 				}
 
 				@Override
-				public GameState visit(Move.DoubleMove move) {
-					currentPlayer.use(move.tickets()).at(move.destination2);
+				public Player visit(Move.DoubleMove move) {
+					Player updatePlayer = currentPlayer.use(move.tickets()).at(move.destination2);
 
 					// moving from source to destination1
 					if (setup.moves.get(log.size())) {
-						updatedLog.add(LogEntry.reveal(move.ticket1, move.destination1));
+						updateLog.add(LogEntry.reveal(move.ticket1, move.destination1));
 					}
 					else {
-						updatedLog.add(LogEntry.hidden(move.ticket1));
+						updateLog.add(LogEntry.hidden(move.ticket1));
 					}
 
 					// moving from destination1 to destination2
-					if (setup.moves.get(updatedLog.size())) {
-						updatedLog.add(LogEntry.reveal(move.ticket2, move.destination2));
+					if (setup.moves.get(updateLog.size())) {
+						updateLog.add(LogEntry.reveal(move.ticket2, move.destination2));
 					}
 					else {
-						updatedLog.add(LogEntry.hidden(move.ticket2));
+						updateLog.add(LogEntry.hidden(move.ticket2));
 					}
-					return new MyGameState(setup, remaining, ImmutableList.copyOf(updatedLog), mrX, detectives);
+					return updatePlayer;
 				}
 			};
 
-			//  remaining pieces in play for the current round
-			for(Piece player : oldRemaining) {
-				if(player != move.commencedBy()) {
-					updatedRemaining.add(player);
-				}
-			}
+			Player updatePlayer = move.accept(visitor);
 
-			//  (and if none remain an initialisation of players for the next round).
-			if(oldRemaining.isEmpty()) {
-				for(Player everyone : allPlayers) {
-					updatedRemaining.add(everyone.piece());
+			// update the new detectives if the updated player is a detective
+			for (Player player : detectives){
+				if (player.piece() == updatePlayer.piece()){
+					updateDetective.add(updatePlayer);
 				}
-			}
-//
-//			Player updatedPlayer = move.accept(visitor);
-//
-//			// update the new detectives if the updated player is a detective
-//			for (Player player : detectives){
-//				if (player.piece() == updatedPlayer.piece()){
-//					updatedDetective.add(updatedPlayer);
-//				}
 //				else {
-//					updatedDetective.add(player);
+//					updateDetective.add(player);
 //				}
-//			}
-//
-//			// check if updatePlayer is mrX
-//			if (updatedPlayer.isMrX()){
-//				updatedMrX = updatedPlayer;
-//			}
-//			else {
-//				updatedMrX = mrX;
-			return null;
-		};
-//
+			}
 
-//			return new MyGameState(setup, remaining, ImmutableList.copyOf(updatedLog), mrX, detectives);
+			// check if updatePlayer is mrX
+			if (updatePlayer.isMrX()){
+				updateMrX = updatePlayer;
+			}
+			else {
+				updateMrX = mrX;
+			}
+
+
+			return new MyGameState(setup, remaining, ImmutableList.copyOf(updateLog), mrX, detectives);
 		}
 
 		/* --------------- HELPER FUNCTIONS   ----------- */
@@ -393,7 +381,7 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			// check if available moves >= 2 and if player has double tix
 			if (player.has(ScotlandYard.Ticket.DOUBLE) && (availableMoves >= 2)){
 				for(Move.SingleMove move1: singleMove) {
-					// player used a tix -> ticket value - 1
+					// player used a tix -> ticketvalue - 1
 					int destination1 = getDestination(move1);
 					Player player2 =  player.use(move1.ticket).at(destination1);
 					Set<Move.SingleMove> secondMove = makeSingleMoves(setup, detectives, player2, destination1);
